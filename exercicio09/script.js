ed6f35c4a076dda5c700b8498253208132896769
Luiz Filipe <luiz_filipe.29@hotmail.com>
2021-12-10 15:15:56 -0300

    Estrutura de repetição
    -While
    -Do While
    -For

---
+++ exercicio09/script.js
@@ -0,0 +1,27 @@
+//estruturas de repetição
+
+for(var i = 1; i <= 10 ; i++){
+
+    if(i % 2 == 0){
+        console.log(i)
+    }
+
+}
+
+var y = 1
+while(y <= 10) {
+
+    if(y % 2 == 0){
+        console.log(y)
+    }
+    y++
+}
+
+var z = 1
+do{
+
+    if(z % 2 == 0){
+        console.log(z)
+    }
+    z++
+}while(z <= 10)
